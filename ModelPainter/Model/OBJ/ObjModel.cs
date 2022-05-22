3c32dbd518d752fea3c6da79451bfdf151e5fcf3
parzivail <colby.newman2000@gmail.com>
2022-05-21 22:43:39 -0400

    add obj model loader (folder was ignored due to VS output gitignore entry)

---
+++ ModelPainter/Model/OBJ/ObjModel.cs
@@ -0,0 +1,13 @@
+﻿using JeremyAnsel.Media.WavefrontObj;
+
+namespace ModelPainter.Model.OBJ;
+
+public record ObjModel(ObjFile ObjFile)
+{
+	public static ObjModel Load(string filename)
+	{
+		var obj = ObjFile.FromFile(filename);
+
+		return new ObjModel(obj);
+	}
+}
