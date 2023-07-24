fdda20c153ece928887e98474eef7c92e586a680
parzivail <colby.newman2000@gmail.com>
2023-07-24 19:41:50 -0400

    remove cafebabe dep

--- ModelPainter/Model/Java/CompiledJavaModel.cs
+++ ModelPainter/Model/Java/CompiledJavaModel.cs
@@ -1,13 +1,11 @@
-﻿using Cafebabe.Class;
-
-namespace ModelPainter.Model.Java;
+﻿namespace ModelPainter.Model.Java;
 
 public class CompiledJavaModel
 {
 	public static CompiledJavaModel Load(string filename)
 	{
-		using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
-		var classFile = JavaClassFile.Read(fs);
+		// using var fs = new FileStream(filename, FileMode.Open, FileAccess.Read, FileShare.Read);
+		// var classFile = JavaClassFile.Read(fs);
 		return null;
 	}
 }
